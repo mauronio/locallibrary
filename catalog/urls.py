8814d579735a4896a78acb1df48261889b6a9836
Mauricio Gomez <mauricio.gomez@gmail.com>
2018-07-21 09:06:36 -0400

    20180721

--- catalog/urls.py
+++ catalog/urls.py
@@ -12,3 +12,7 @@ urlpatterns = [
 urlpatterns += [   
     url(r'^mybooks/$', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
 ]
+
+urlpatterns += [   
+    url(r'^book/(?P<pk>[-\w]+)/renew/$', views.renew_book_librarian, name='renew-book-librarian'),
+]
